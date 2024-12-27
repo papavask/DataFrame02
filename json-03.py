c617d3762d9c209d6004d74c1861c9b5dc6d9bac
papavask <44700690+papavask@users.noreply.github.com>
2024-12-27 21:50:20 +0200

    Update json-03.py

--- json-03.py
+++ json-03.py
@@ -2,13 +2,10 @@ import json
 import requests
 import streamlit as st
 import pandas as pd
-#from pandas.io.json import json_normalize
 
 # Function to fetch a JSON file from GitHub
 def fetch_json_from_github(url):
     response = requests.get(url)
-    #st.write("Status", response.status_code)
-    #st.write(response.text)
     response.raise_for_status()  # Check for HTTP request errors
     return response.text  # Return parsed JSON
 
@@ -16,32 +13,22 @@ def fetch_json_from_github(url):
 def main():
     st.title("Speculations...")
     df = pd.DataFrame({})
-    # Input for the GitHub URL
-    #url = st.text_input("Enter the GitHub raw JSON URL", "")
     url = 'https://raw.githubusercontent.com/papavask/Dataframe01/main/page07.txt'
     if url:
         try:
             # Fetch the JSON data from the provided URL
             data = fetch_json_from_github(url)
-        except Exception as e:
-            st.error(f"Error: {e}")
-
-        try:
-            # Display data
-            #st.write(data)
-            #jstr = json.loads(data)
-            astr = data.splitlines()
-            for poet in astr:
-                #st.write("List>>>>",poet)  # Display each poet's information
-                jstr = json.loads(poet)
-                #st.write("Json>>>>", jstr)
-                dt = pd.json_normalize(jstr)
-                #st.write("Norm >>>>>",dt)
-                #df.append(dt, ignore_index=True)  
-                if df.empty:
-                    df = dt
-                else:       
-                    df = pd.concat([df, dt], ignore_index=True)
+                    try:
+                        astr = data.splitlines()
+                        for spec in astr:
+                            jstr = json.loads(spec)
+                            dt = pd.json_normalize(jstr) 
+                            if df.empty:
+                                df = dt
+                            else:       
+                                df = pd.concat([df, dt], ignore_index=True)
+                    except Exception as e:
+                        st.error(f"Error: {e}")
         except Exception as e:
             st.error(f"Error: {e}")
 
